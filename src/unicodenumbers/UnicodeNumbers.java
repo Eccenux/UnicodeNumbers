4e57657cc9fc7740670e53d06b375a8de449a111
Eccenux <eccenux+git@gmail.com>
2016-09-06 22:51:29 +0200

    Unicode Numbers Formatter/Converter

---
+++ src/unicodenumbers/UnicodeNumbers.java
@@ -0,0 +1,17 @@
+package unicodenumbers;
+
+/**
+ * Just a testing area.
+ *
+ * @author Maciej Nux Jaros
+ */
+public class UnicodeNumbers {
+
+	/**
+	 * @param args the command line arguments
+	 */
+	public static void main(String[] args) {
+		// TODO code application logic here
+	}
+
+}
